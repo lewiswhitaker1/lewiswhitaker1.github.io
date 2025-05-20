c744e97482f9abebc1b2835ce1f7490d85201bc2
Lewis <l.whitaker@activewindowfilms.co.uk>
2025-05-20 10:32:30 +0100

    testing

--- js/main.js
+++ js/main.js
@@ -1,24 +1,19 @@
 $(document).ready(function() {
     console.log("Main JavaScript file loaded.");
 
-    // --- Common Cropper Initialization --- 
-    window.cropper = null; // Made global
-    // Product-specific image and upload elements will be handled in their respective files
+    window.cropper = null;
 
-    window.initializeCropper = function(imageElement, options = {}) { // Made global
+    window.initializeCropper = function(imageElement, options = {}) {
         if (window.cropper) {
             window.cropper.destroy();
         }
         window.cropper = new Cropper(imageElement, {
-            aspectRatio: 1, // Default aspect ratio, can be overridden by product page
-            viewMode: 1,
+            aspectRatio: 1,
+            viewMode: 2,
             responsive: true,
             autoCropArea: 0.8,
-            ...options // Spread any product-specific options
+            ...options
         });
         imageElement.style.display = 'block';
     }
-
-    // Product-specific logic has been moved to js/product1.js and js/product2.js
-    // Ensure those files are included in the respective HTML pages after main.js
 });
