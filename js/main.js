06d5f36927d34812c06dee68aa0b3fc1b0a39314
Lewis <l.whitaker@activewindowfilms.co.uk>
2025-05-20 10:45:35 +0100

    fixed cropping

--- js/main.js
+++ js/main.js
@@ -10,8 +10,16 @@ $(document).ready(function() {
         window.cropper = new Cropper(imageElement, {
             aspectRatio: 1,
             viewMode: 2,
+            dragMode: 'move',
             responsive: true,
             autoCropArea: 0.8,
+            restore: false,
+            guides: true,
+            center: true,
+            highlight: false,
+            cropBoxMovable: true,
+            cropBoxResizable: true,
+            toggleDragModeOnDblclick: false,
             ...options
         });
         imageElement.style.display = 'block';
